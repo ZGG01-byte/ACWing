7cd2a232594ba043563fe4b0c8babc13b0a9bffe
chl <1612029553@qq.com>
2022-04-22 16:24:47 +0800

    first-commit

--- acwingCode/src/main/java/com/chl/test/Test.java
+++ acwingCode/src/main/java/com/chl/test/Test.java
@@ -3,6 +3,7 @@ package com.chl.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello git");
     }
 }
 
